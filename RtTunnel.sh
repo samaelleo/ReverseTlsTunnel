70582bf31d1e6c585177c6c88cff68c675c2c6e1
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-27 21:19:50 +0330

    update RtTunnel.sh

--- RtTunnel.sh
+++ RtTunnel.sh
@@ -388,16 +388,6 @@ compile() {
     echo "RTT file is located at: ReverseTlsTunnel/dist"
 }
 
-display_tunnel_status() {
-    echo "Multiport is:"
-    check_service_status "tunnel.service"
-}
-
-display_lbtunnel_status() {
-    echo "Load-Balancer is:"
-    check_service_status "lbtunnel.service"
-}
-
 
 #ip & version
 myip=$(hostname -I | awk '{print $1}')
@@ -408,8 +398,6 @@ clear
 echo "By --> Peyman * Github.com/Ptechgithub * "
 echo "Your IP is: ($myip) "
 echo ""
-display_tunnel_status
-display_lbtunnel_status
 echo " --------#- Reverse Tls Tunnel -#--------"
 echo "1) Install (Multiport)"
 echo "2) Uninstall (Multiport)"
